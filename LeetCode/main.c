81db2ae86d3189317eb66b3afd49e60d2f479669
wanyakun <wanyakun@vip.qq.com>
2020-10-27 16:21:42 +0800

    update 322

--- LeetCode/main.c
+++ LeetCode/main.c
@@ -6,18 +6,9 @@
 //
 
 #include <stdio.h>
-#include <stdlib.h>
-#include "NO322.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    int* coins = calloc(1, sizeof(int));
-    coins[0] = 2;
-//    coins[1] = 2;
-//    coins[2] = 5;
-    int coinsSize = 1;
-    int amount = 3;
-    int result = coinChange2(coins, coinsSize, amount);
-    printf("coinChange result: %d \n", result);
+    printf("Hello, World!\n");
     return 0;
 }
