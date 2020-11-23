a85376edcf6f56daba0fd5b3ff61bbab92588320
wanyakun <wanyakun@vip.qq.com>
2020-11-23 17:57:33 +0800

    Add NO62

---
+++ NO62/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  NO62
+//
+//  Created by wanyakun on 2020/11/23.
+//
+
+#include <stdio.h>
+#include "NO62.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int res = uniquePaths(3, 7);
+    printf("%d \n", res);
+    return 0;
+}
