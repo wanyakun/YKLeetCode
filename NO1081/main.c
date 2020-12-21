e094e70d5eff73a22a1de1d35f027c6a4a2e83e6
wanyakun <wanyakun@vip.qq.com>
2020-12-21 11:10:53 +0800

    add NO1081

---
+++ NO1081/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  NO1081
+//
+//  Created by wanyakun on 2020/12/21.
+//
+
+#include <stdio.h>
+#include "NO1081.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    char* s = "cdadabcc";
+    char* res = smallestSubsequence(s);
+    printf("%s \n", res);
+    return 0;
+}
